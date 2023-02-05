9c0b1c4059f209dc6e683bd142cb6187bb36e135
Chitipiralla Harshith Sai Reddy <113248597+chitipirall@users.noreply.github.com>
2023-02-05 15:58:17 +0530

    Add files via upload

--- binary search.cpp
+++ binary search.cpp
@@ -1,24 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int a[]={10,20,30,40,50};
-	int low=0,high=4,mid,x;
-	printf("enter a value:");
-	scanf("%d",&x);
-	while(low <= mid)
-	{
-		mid=(low + high)/2;
-		if(a[mid]>x)
-		{
-			high=mid-1;
-		}
-		else if(a[mid]<x)
-		{
-			low=mid+1;
-		}
-		else if(a[mid]==x)
-		{
-			printf("%d is answer",a[mid]);
-		}
-	}
+  int c, first, last, middle, n, search, array[100];
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+
+  printf("Enter value to find\n");
+  scanf("%d", &search);
+
+  first = 0;
+  last = n - 1;
+  middle = (first+last)/2;
+
+  while (first <= last) {
+    if (array[middle] < search)
+      first = middle + 1;
+    else if (array[middle] == search) {
+      printf("%d found at location %d.\n", search, middle+1);
+      break;
+    }
+    else
+      last = middle - 1;
+
+    middle = (first + last)/2;
+  }
+  if (first > last)
+    printf("Not found! %d isn't present in the list.\n", search);
+
+  return 0;
 }
