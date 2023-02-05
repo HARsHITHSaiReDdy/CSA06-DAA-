1cbaa890f67d1eb7bc31542c532d142c39650669
Chitipiralla Harshith Sai Reddy <113248597+chitipirall@users.noreply.github.com>
2023-02-05 16:04:25 +0530

    Add files via upload

---
+++ reverse string.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  char str[1000], rev[1000];
+  int i, j, count = 0;
+  scanf("%s", str);
+  printf("\nString Before Reverse: %s", str);
+  while (str[count] != '\0')
+  {
+    count++;
+  }
+  j = count - 1;
+  {
+    rev[i] = str[j];
+    j--;
+  }
+  printf("\nString After Reverse: %s", rev);
+}
